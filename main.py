1b0233b2d8b82fdf4ec119e732fe648e5f96ae97
Tiago Almeida <tiagomeloalmeida@ua.pt>
2019-10-10 11:20:48 +0100

    args namespace fix

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     parser.add_argument("--mode", help="Runing mode of the pipeline",
                         choices=['train', 'inference'])
     parser.add_argument("--query", help="Runing mode of the pipeline")
-    parser.add_argument("--validate_file", help="Validation file with queries")
+    parser.add_argument("--validation_file", help="Validation file with queries")
     parser.add_argument("config", help="configuration file with the instructions, must be in json or yaml format")
 
     args = parser.parse_args()
