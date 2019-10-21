a7245740e9fd411c276c80f4d89a383d7283cdf8
Tiago Almeida <tiagomeloalmeida@ua.pt>
2019-10-21 15:58:27 +0100

    remove thesis notebooks

--- masters_thesis_support_CODE/_old_preprocess/path_helper.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def resolve_path(path):
-    if os.path.basename(os.getcwd()) == "preprocess":
-        return os.path.join("..", "data", path)
-    elif os.path.basename(os.getcwd()) == "bioASQ-taskb":
-        return os.path.join(".", "data", path)
-    else:
-        raise RuntimeError("current folder " + os.path.basename(os.getcwd()) + " is invalid")
--- masters_thesis_support_CODE/_old_preprocess/pubmeb_rep.py
+++
@@ -1,9 +0,0 @@
-
-
-import pubmed_parser as pp
-
-path = "..\\data\\medline17n0001.xml"
-
-pubmed_dict = pp.parse_medline_xml(path)
-
-print(pubmed_dict)
